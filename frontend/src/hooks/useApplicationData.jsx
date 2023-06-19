de526cff30838e23eeb6900ca6e5d285146dfa60
Bastien Moy <moy.bastien@gmail.com>
2023-06-19 13:48:49 -0700

    refactoring hook to use reducer

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -1,26 +1,58 @@
-import { React, useState} from "react";
+import { React, useState } from "react";
 
-const useAppicationData = () => {
+const useApplicationData = () => {
 
-  const [favPhotoIds,setfavPhotoIds] = useState([]);
+  // const [favPhotoIds,setfavPhotoIds] = useState([]);
 
-  
-  const [showModal, setshowModal] = useState(false);
-  const [modalData, setmodalData] = useState({});
-  
-  
-  const toggleFavLiked = (photoid) => {
-    const photoIdIndex = favPhotoIds.indexOf(photoid);
-    if (photoIdIndex === -1) {
-      setfavPhotoIds([...favPhotoIds, photoid])
-    } else {
-      const removedFavPhotoIds = [...favPhotoIds];
-      removedFavPhotoIds.splice(photoIdIndex,1);
-      setfavPhotoIds(removedFavPhotoIds);
+
+  // const [showModal, setshowModal] = useState(false);
+  // const [modalData, setmodalData] = useState({});
+
+
+  // const toggleFavLiked = (photoid) => {
+  //   const photoIdIndex = favPhotoIds.indexOf(photoid);
+  //   if (photoIdIndex === -1) {
+  //     setfavPhotoIds([...favPhotoIds, photoid])
+  //   } else {
+  //     const removedFavPhotoIds = [...favPhotoIds];
+  //     removedFavPhotoIds.splice(photoIdIndex,1);
+  //     setfavPhotoIds(removedFavPhotoIds);
+  //   }
+  // }
+
+  const ACTIONS = {
+    FAV_PHOTO_TOGGLE: 'FAV_PHOTO_TOGGLE',
+    SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+    SELECT_PHOTO: 'SELECT_PHOTO',
+    TOGGLE_MODAL: 'TOGGLE_MODAL'
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'FAV_PHOTO_TOGGLE':
+        if (state.includes(action.id)) {
+          const filtered = state.filter((favPhotoId) => favPhotoId !== action.id);
+          return filtered;
+        }
+        return [...state, action.id];
+      case 'SET_PHOTO_DATA':
+        return action.data;
+      case 'SELECT_PHOTO':
+        return action.array.find((photo) => (photo.id === action.id));
+      case 'TOGGLE_MODAL':
+        return !state;
+      default:
+        return state;
     }
   }
 
+  const [favPhotosID, updateToFavPhotosIDs] = useReducer(reducer, []);
+  const [photoSelected, choosePhotoSelected] = useReducer(reducer, {});
+  const [openModal, toggleModal] = useReducer(reducer, false); 
+
+
   const state = {
+    photoSelected,
     showModal,
     favPhotoIds,
     modalData,
@@ -28,10 +60,11 @@ const useAppicationData = () => {
 
   return {
     state,
+    choosePhotoSelected,
     toggleFavLiked,
     setshowModal,
     setmodalData
   };
 };
 
-export default useAppicationData;
+export default useApplicationData;
